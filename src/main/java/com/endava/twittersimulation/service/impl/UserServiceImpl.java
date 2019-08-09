5f7e2686dd50649f8fa889fcb4d20a8ab91cdbc6
AleksandarStojanovikj <aleksandar_sangi@hotmail.com>
2019-08-09 17:04:51 +0200

    Implement userService logic

--- src/main/java/com/endava/twittersimulation/service/impl/UserServiceImpl.java
+++ src/main/java/com/endava/twittersimulation/service/impl/UserServiceImpl.java
@@ -1,10 +1,14 @@
 package com.endava.twittersimulation.service.impl;
 
+import com.endava.twittersimulation.exceptions.UserAlreadyExistsException;
+import com.endava.twittersimulation.exceptions.UserDoesNotExistException;
 import com.endava.twittersimulation.model.User;
 import com.endava.twittersimulation.repository.UserRepository;
 import com.endava.twittersimulation.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -18,12 +22,24 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User save(User user) {
-        return null;
+        Optional<User> existingUser = userRepository.findById(user.getId());
+
+        if (existingUser.isPresent()) {
+            throw new UserAlreadyExistsException();
+        }
+
+        return userRepository.save(user);
     }
 
     @Override
     public void changePassword(Long userId, String oldPassword, String newPassword) {
+        Optional<User> existingUser = userRepository.findById(userId);
 
+        if (!existingUser.isPresent()) {
+            throw new UserDoesNotExistException();
+        }
+
+        existingUser.get().changePassword(oldPassword, newPassword);
     }
 
     @Override
