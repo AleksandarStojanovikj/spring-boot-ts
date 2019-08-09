18ca82991e0fedd1ed8f10fd9dd7d401b497b131
AleksandarStojanovikj <aleksandar_sangi@hotmail.com>
2019-08-09 15:11:35 +0200

    Add repository annotations

--- src/main/java/com/endava/twittersimulation/repository/TweetRepository.java
+++ src/main/java/com/endava/twittersimulation/repository/TweetRepository.java
@@ -2,6 +2,8 @@ package com.endava.twittersimulation.repository;
 
 import com.endava.twittersimulation.model.Tweet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
 }
